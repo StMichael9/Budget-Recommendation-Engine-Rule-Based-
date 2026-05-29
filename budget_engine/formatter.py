7aac78a6082087564acdce0bba635e2f63836bbc
St Michael Egenamba <michaelegenamba@gmail.com>
2026-05-29 09:24:14 -0700

    Finished formatter.py

--- budget_engine/formatter.py
+++ budget_engine/formatter.py
@@ -8,14 +8,23 @@ preparing data for future API responses"""
 
 import json
 
+# Squishes data into a tight text line so computers can send it fast
 def to_json(data):
     # convert Python dict to JSON string
-    pass
+    data = json.dumps(data)
+    return data
 
+# Adds lines and spaces to the text so humans can read it easily
 def to_pretty_console(data):
     # convert Python dict to formatted console output
-    pass
+    data = json.dumps(data, indent = 4)
+    return data
 
 def to_api_response(data):
     # prepare standardized API response dict
-    pass
+    response = {
+        "status": "OK",
+        "message": "All right",
+        "data": data
+    }
+    return response
